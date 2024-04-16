0cd07a4b500d612920fa497845b5a3979feb712d
AndrewHyland757 <hyland757@gmail.com>
2024-04-16 07:21:23 +0000

    Add url routes for the apps. Set up templates folder in project level and template folders in each app.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.shortcuts import render
+from django.http import HttpResponse
+#from .models import Album
+
+
+def products(request):
+
+    #products = Album.objects.all()
+    context = {
+        #"albums": products
+    }
+
+    return render(request, 'products/products.html', context)
+
+
+def product(request, pk):
+
+    #product = Album.objects.get(id=pk)
+    context = {
+        #"album": product
+    }
+
+    return render(request, 'products/product.html', context)
