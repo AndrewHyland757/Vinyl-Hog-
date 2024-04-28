903e99495375b52d5091a2b6236c01f0d87f51a5
AndrewHyland757 <hyland757@gmail.com>
2024-04-28 23:04:53 +0000

    Update checkout.html css

--- static/js/home.js
+++
@@ -1,9 +0,0 @@
-
-
-//var containerCoverImg = document.getElementsByClassName("right-2")[0];
-
-//var coverImg = document.getElementsByClassName("cover-img")[0];
-
-//coverImg.style.width = containerCoverImg.offsetWidth / 2 + "px";
-
-//coverImg.style.height = containerCoverImg.offsetHeight / 2 + "px";
