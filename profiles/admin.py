086c6a27a12be2cf6a2f6d8c16c60e42d0d75693
AndrewHyland757 <hyland757@gmail.com>
2024-04-30 09:46:45 +0000

    Fix UserProfile issue

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-
-from .models import Wishlist
-
+from .models import Wishlist, UserProfile
 # Register your models here.
 
+
 admin.site.register(Wishlist)
+admin.site.register(UserProfile)
