8cad921cdc2cb37dcb0371cbc1e73da3d46dc9c3
AndrewHyland757 <hyland757@gmail.com>
2024-07-02 17:16:25 +0000

    Add token

--- vinyl_hog/settings.py
+++ vinyl_hog/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
     
 DEBUG = True
-CSRF_TRUSTED_ORIGINS = ['https://8000-andrewhyland75-vinylhog-adqzwwz25b6.ws-eu114.gitpod.io/']
+CSRF_TRUSTED_ORIGINS = ['https://8000-andrewhyland75-vinylhog-adqzwwz25b6.ws-eu114.gitpod.io']
 
 ALLOWED_HOSTS = ['vinyl-hog-db2fc2977a10.herokuapp.com', '8000-andrewhyland75-vinylhog-adqzwwz25b6.ws-eu114.gitpod.io']
 
